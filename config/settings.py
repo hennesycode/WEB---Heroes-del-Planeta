43e0e784ecd30c07a190f84d3d1a6aa309105467
Carlos Perdomo <cpcarlosperdomo@hotmail.com>
2025-07-03 09:17:19 -0500

    Configuración para despliegue en Render

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+
+
 import os
 
 from pathlib import Path
@@ -80,11 +82,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgres://...',  # Puedes dejarlo así, lo sobreescribiremos con la variable de entorno en Render.
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
